a51361cf8ac46ff781cad589ad9a55d73d2e64f6
Felix Reihl <felix.reihl@gmail.com>
2015-03-16 18:42:13 +0100

    now with a form

--- yesodMissingNo/Handler/Game.hs
+++ yesodMissingNo/Handler/Game.hs
@@ -1,43 +1,39 @@
 module Handler.Game where
 
 import Import
-import Yesod.Form.Bootstrap3
-
-data Input = Input Text deriving Show
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, withSmallInput)
 
 getGameR :: Text -> Handler Html
 getGameR id = do
-    defaultLayout [whamlet|Welcome to the game, #{id}|]
---     (formWidget, formEnctype) <- generateFormPost gameForm
---     let submission = Nothing :: Maybe (FileInfo, Text)
---         handlerName = "getGameR" :: Text
---     defaultLayout $ do
---         aDomId <- newIdent
---         setTitle "Welcome to the game!"
---         -- $(widgetFile "game")
---         [whamlet|Welcome to the game, #{id}|]
+    (formWidget, formEnctype) <- generateFormPost gameForm
+    let submission = Nothing :: Maybe (Text)
+        -- handlerName = "getGameR" :: Text
+    defaultLayout $ do
+        -- aDomId <- newIdent
+        setTitle "Welcome to the game!"
+        -- $(widgetFile "game")
+        [whamlet|Welcome to the game, #{id}|]
+        [whamlet|
+            Enter Text:
+            <form method=post action=@{HomeR} enctype=#{formEnctype}>
+            ^{formWidget}
+            <button>Submit
+            |]
 
--- postGameR :: Text -> Handler Html
--- postGameR id = do
---     ((result, formWidget), formEnctype) <- runFormPost gameForm
---     let handlerName = "postGameR" :: Text
---         submission = case result of
---             FormSuccess res -> Just res
---             _ -> Nothing
---     defaultLayout $ do
---         aDomId <- newIdent
---         setTitle "Welcome to the game!"
---         -- $(widgetFile "game")
---         [whamlet|Thanks for posting, #{submission}|]
--- 
--- gameAForm :: AForm Handler Input
--- gameAForm = Input
---     <$> areq textField "Input" Nothing
--- 
--- gameForm :: Html -> MForm Handler (FormResult Input, Widget)
--- gameForm = renderTable $ Input $
---     areq textField "Input" Nothing
 
--- gameForm :: AForm Handler Input
--- gameForm = Input
---     <$> areq textField (bfs ("Name" :: Text)) Nothing
+postGameR :: Text -> Handler Html
+postGameR id = do
+    ((result, formWidget), formEnctype) <- runFormPost gameForm
+    -- let handlerName = "postGameR" :: Text
+    case result of
+        FormSuccess res -> defaultLayout [whamlet|Thanks for posting, #{res}|]
+        _ -> defaultLayout [whamlet|
+            Try again:
+            <form method=post action=@{HomeR} enctype=#{formEnctype}>
+            ^{formWidget}
+            <button>Submit
+            |]
+
+gameForm :: Form Text
+gameForm = renderBootstrap3 BootstrapBasicForm $
+    areq textField "Input" Nothing
