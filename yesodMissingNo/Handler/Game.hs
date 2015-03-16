b39a578c9446f9685200500ddbf86df02fb15502
Felix Reihl <felix.reihl@gmail.com>
2015-03-16 23:39:24 +0100

    submit button finally working

--- yesodMissingNo/Handler/Game.hs
+++ yesodMissingNo/Handler/Game.hs
@@ -6,33 +6,22 @@ import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, withSm
 getGameR :: Text -> Handler Html
 getGameR id = do
     (formWidget, formEnctype) <- generateFormPost gameForm
-    let submission = Nothing :: Maybe (Text)
-        -- handlerName = "getGameR" :: Text
+    let handlerName = "getGameR" :: Text
+        submission = Nothing :: Maybe Text
     defaultLayout $ do
-        -- aDomId <- newIdent
         setTitle "Welcome to the game!"
-        -- $(widgetFile "game")
-        [whamlet|Welcome to the game, #{id}|]
-        [whamlet|
-            Enter Text:
-            <form method=post action=@{HomeR} enctype=#{formEnctype}>
-            ^{formWidget}
-            <button>Submit
-            |]
-
+        $(widgetFile "game")
 
 postGameR :: Text -> Handler Html
 postGameR id = do
     ((result, formWidget), formEnctype) <- runFormPost gameForm
-    -- let handlerName = "postGameR" :: Text
-    case result of
-        FormSuccess res -> defaultLayout [whamlet|Thanks for posting, #{res}|]
-        _ -> defaultLayout [whamlet|
-            Try again:
-            <form method=post action=@{HomeR} enctype=#{formEnctype}>
-            ^{formWidget}
-            <button>Submit
-            |]
+    let handlerName = "postGameR" :: Text
+        submission = case result of
+            FormSuccess res -> Just res
+            _ -> Nothing
+    defaultLayout $ do
+        setTitle "Welcome to the game!"
+        $(widgetFile "game")
 
 gameForm :: Form Text
 gameForm = renderBootstrap3 BootstrapBasicForm $
