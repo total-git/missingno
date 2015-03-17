696fa61b4346d5b816bbc963a7327409d7819034
Felix Reihl <felix.reihl@gmail.com>
2015-03-17 15:48:34 +0100

    put the parser functions into Parser.hs and made the parser case-insensitive

--- yesodMissingNo/Handler/Game.hs
+++ yesodMissingNo/Handler/Game.hs
@@ -1,8 +1,8 @@
 module Handler.Game where
 
 import Import
+import Parser
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Data.Text (strip)
 
 getGameR :: Text -> Handler Html
 getGameR playerId = do
@@ -25,36 +25,3 @@ postGameR playerId = do
 gameForm :: Form Text
 gameForm = renderBootstrap3 BootstrapBasicForm $
     areq textField "Input" Nothing
-
-data Input = Input { act :: Maybe Action,
-                     obj :: Maybe Text
-                   } deriving (Show, Eq)
-
-data Action = Go | Use | Examine | PickUp | Inventory | LookAround deriving (Show, Eq)
--- TODO add more actions
-action :: Text -> Maybe Action
-action txt | oelem txt ["go", "move", "run", "walk"]
-             = Just Go
-           | oelem txt ["use", "utilize", "utilise", "make use of"]
-             = Just Use
-           | oelem txt ["examine", "look at", "inspect", "view", "watch"]
-             = Just Examine
-           | oelem txt ["pick up", "get", "take", "gather", "grab"]
-             = Just PickUp
-           | oelem txt ["inventory", "pocket", "pockets", "bag", "bags", "rucksack", "backpack"]
-             = Just Inventory
-           | oelem txt ["look around", "look about", "look round", "scout", "glance around"]
-             = Just LookAround
-           | otherwise
-             = Nothing
-
-parseInput :: Text -> Input
-parseInput txt = parseInputWords (words txt) ""
-  where
-    parseInputWords :: [Text] -> Text -> Input
-    parseInputWords (w:ws) cs =
-        case action cs of
-            Just act -> Input (Just act) (Just $ unwords $ w:ws)
-            Nothing  -> parseInputWords ws $ strip $ unwords [cs,w]
-    parseInputWords [] cs = Input (action cs) Nothing
-
---
+++ yesodMissingNo/Parser.hs
@@ -0,0 +1,37 @@
+module Parser where
+
+import Import
+import Data.Text (strip)
+
+data Input = Input { act :: Maybe Action,
+                     obj :: Maybe Text
+                   } deriving (Show, Eq)
+
+data Action = Go | Use | Examine | PickUp | Inventory | LookAround deriving (Show, Eq)
+-- TODO add more actions
+action :: Text -> Maybe Action
+action txt | oelem (toLower txt) ["go", "move", "run", "walk"]
+             = Just Go
+           | oelem (toLower txt) ["use", "utilize", "utilise", "make use of"]
+             = Just Use
+           | oelem (toLower txt) ["examine", "look at", "inspect", "view", "watch"]
+             = Just Examine
+           | oelem (toLower txt) ["pick up", "get", "take", "gather", "grab"]
+             = Just PickUp
+           | oelem (toLower txt) ["inventory", "pocket", "pockets", "bag", "bags", "rucksack", "backpack"]
+             = Just Inventory
+           | oelem (toLower txt) ["look around", "look about", "look round", "scout", "glance around"]
+             = Just LookAround
+           | otherwise
+             = Nothing
+
+parseInput :: Text -> Input
+parseInput txt = parseInputWords (words txt) ""
+  where
+    parseInputWords :: [Text] -> Text -> Input
+    parseInputWords (w:ws) cs =
+        case action cs of
+            Just act -> Input (Just act) (Just $ unwords $ w:ws)
+            Nothing  -> parseInputWords ws $ strip $ unwords [cs,w]
+    parseInputWords [] cs = Input (action cs) Nothing
+
