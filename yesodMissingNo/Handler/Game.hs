74849e6738d18d0375b45bdca09c035861044cf3
Felix Reihl <felix.reihl@gmail.com>
2015-03-17 15:37:11 +0100

    parsing input correctly

--- yesodMissingNo/Handler/Game.hs
+++ yesodMissingNo/Handler/Game.hs
@@ -1,23 +1,22 @@
 module Handler.Game where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, withSmallInput)
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Data.Text (strip)
 
 getGameR :: Text -> Handler Html
-getGameR id = do
+getGameR playerId = do
     (formWidget, formEnctype) <- generateFormPost gameForm
-    let handlerName = "getGameR" :: Text
-        submission = Nothing :: Maybe Text
+    let submission = Nothing :: Maybe Text
     defaultLayout $ do
         setTitle "Welcome to the game!"
         $(widgetFile "game")
 
 postGameR :: Text -> Handler Html
-postGameR id = do
+postGameR playerId = do
     ((result, formWidget), formEnctype) <- runFormPost gameForm
-    let handlerName = "postGameR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
+    let submission = case result of
+            FormSuccess res -> Just $ show $ parseInput res
             _ -> Nothing
     defaultLayout $ do
         setTitle "Welcome to the game!"
@@ -26,3 +25,36 @@ postGameR id = do
 gameForm :: Form Text
 gameForm = renderBootstrap3 BootstrapBasicForm $
     areq textField "Input" Nothing
+
+data Input = Input { act :: Maybe Action,
+                     obj :: Maybe Text
+                   } deriving (Show, Eq)
+
+data Action = Go | Use | Examine | PickUp | Inventory | LookAround deriving (Show, Eq)
+-- TODO add more actions
+action :: Text -> Maybe Action
+action txt | oelem txt ["go", "move", "run", "walk"]
+             = Just Go
+           | oelem txt ["use", "utilize", "utilise", "make use of"]
+             = Just Use
+           | oelem txt ["examine", "look at", "inspect", "view", "watch"]
+             = Just Examine
+           | oelem txt ["pick up", "get", "take", "gather", "grab"]
+             = Just PickUp
+           | oelem txt ["inventory", "pocket", "pockets", "bag", "bags", "rucksack", "backpack"]
+             = Just Inventory
+           | oelem txt ["look around", "look about", "look round", "scout", "glance around"]
+             = Just LookAround
+           | otherwise
+             = Nothing
+
+parseInput :: Text -> Input
+parseInput txt = parseInputWords (words txt) ""
+  where
+    parseInputWords :: [Text] -> Text -> Input
+    parseInputWords (w:ws) cs =
+        case action cs of
+            Just act -> Input (Just act) (Just $ unwords $ w:ws)
+            Nothing  -> parseInputWords ws $ strip $ unwords [cs,w]
+    parseInputWords [] cs = Input (action cs) Nothing
+
