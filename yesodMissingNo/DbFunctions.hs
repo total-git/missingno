b3fbf3b1738f96a2af3ee1314fd3082e1f71769c
Eva Braß <eva.brass@gmx.de>
2015-03-23 16:23:15 +0100

    new DB functions

--- yesodMissingNo/DbFunctions.hs
+++ yesodMissingNo/DbFunctions.hs
@@ -3,6 +3,9 @@ module DbFunctions where
 import Import
 import Database.Persist.Sql
 
+lookAtArea :: Int64 -> Handler (Maybe Area)
+lookAtArea areaId = do
+    runDB $ get $ toSqlKey areaId
 
 lookAtItemByUnique :: Text -> Int64 -> Handler (Maybe (Entity Item))
 lookAtItemByUnique name areaId = do
@@ -11,3 +14,11 @@ lookAtItemByUnique name areaId = do
 showItemsInArea :: Int64 -> Handler [Entity Item]
 showItemsInArea areaId = do
     runDB $ selectList [ItemArea_id ==. toSqlKey areaId] []
+
+getPlayer :: Text -> Handler (Maybe (Entity Player_status))
+getPlayer urlHash = do
+    runDB $ selectFirst [Player_statusUrlHash ==. urlHash] []
+
+updateArea :: Int64 -> Text -> Handler ()
+updateArea newArea urlHash = do
+    runDB $ updateWhere [Player_statusUrlHash ==. urlHash] [Player_statusArea_id =. (toSqlKey newArea)]
